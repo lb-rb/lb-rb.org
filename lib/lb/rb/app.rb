66d7703565aedc069d292a71611d95240798e9b8
Firas Zaidan <firas@zaidan.de>
2018-05-06 22:48:02 +0200

    Refactor project to use lb-project

--- lib/lb/rb/app.rb
+++ lib/lb/rb/app.rb
@@ -2,7 +2,9 @@
 module LB
   module RB
     # Main routing entry point
-    class App < LB::RB::Route
+    class App < LB::Project::App
+      LB::Project::Route.setup(self)
+
       route do |r|
         r.i18n_set_locale_from(:http)
 
